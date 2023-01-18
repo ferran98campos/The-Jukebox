87513d303020aeee60e25bc6bc9e184e5d77168c
Ferran Campos <73848801+ferran98campos@users.noreply.github.com>
2023-01-18 15:58:34 +0100

    estilos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,10 +10,14 @@ export class AppComponent {
 
   private lights:Boolean;
   private scrollInterval:number;
+  private lastHover:Date;
+  private songPlaying:Boolean;
 
   constructor(){
     this.lights = false;
     this.scrollInterval = 0; 
+    this.lastHover = new Date();
+    this.songPlaying = false;
   }
 
   //Adds an event listener to the Spotify Button
@@ -38,12 +42,29 @@ export class AppComponent {
     const rightArrow = document.getElementById('queue-right-arrow');
     rightArrow?.addEventListener('mouseover', this.scrollPlaylist.bind(this, true));
     rightArrow?.addEventListener('mouseout', this.stopScroll.bind(this));
+
+    //Show Next Song Info
+    const queueElements = this.getAllSpanElementsFromElement('songs-queue');
+    if(queueElements!=null){
+      for (var i = 0; i < queueElements.length; i++) {
+        queueElements[i]?.addEventListener('mouseover', this.showNextSongInfo.bind(this, queueElements[i]));
+        queueElements[i]?.addEventListener('mouseout', this.notShowNextSongInfo.bind(this, queueElements[i]));
+        queueElements[i]?.addEventListener('click', this.selectSong.bind(this, queueElements[i]));
+      }
+    }
+
+    //Pause/Play song functionality
+    const songImage = document.getElementById('playing-song-image');
+    songImage?.addEventListener('click', this.playSong.bind(this, songImage));
   }
 
   getAllInputElementsFromElement(parentId: string){
     return document.getElementById(parentId)?.getElementsByTagName('input');
   }
 
+  getAllSpanElementsFromElement(parentId: string){
+    return document.getElementById(parentId)?.getElementsByTagName('span');
+  }
 
   turnLights(): void{
     //Define variables
@@ -89,19 +110,103 @@ export class AppComponent {
 
   //Scroll playlist. Direction: true moves it to right, Direction: false moves it left
   scrollPlaylist(direction: boolean): void{
-    
-      this.scrollInterval = window.setInterval(() => {
-        var playlist = document.getElementById('songs-queue');
-        var amount = ((direction) ? 150 : -150);
-        if(document.getElementById('songs-queue') != null && document.getElementById('songs-queue')?.scrollLeft != null){
-          document.getElementById('songs-queue')!.scrollLeft = document.getElementById('songs-queue')!.scrollLeft + amount;
-          console.log(document.getElementById('songs-queue')?.scrollLeft);
-      }}, 120);
-    }
+    if(this.lights){
+      var playlist = document.getElementById('songs-queue');
+      playlist!.classList.remove("not-moving");
+      playlist!.classList.add("moving");
   
-
+        this.scrollInterval = window.setInterval(() => {
+          var playlist = document.getElementById('songs-queue');
+          var amount = ((direction) ? 5 : -5);
+            playlist!.scrollLeft +=  amount;
+        }, 3);
+    }
+  }
+  
   stopScroll(): void{
-    console.log('left');
-    window.clearInterval(this.scrollInterval);
+    if(this.lights){
+      var playlist = document.getElementById('songs-queue');
+      playlist!.classList.remove("moving");
+      playlist!.classList.add("not-moving");
+      window.clearInterval(this.scrollInterval);
+    }
+  }
+
+  showNextSongInfo(element: HTMLSpanElement): void{
+    if(this.lights){
+      var nextSongInfo = document.getElementById('next-song-info');
+      nextSongInfo!.classList.remove("not-showing");
+      nextSongInfo!.classList.add("showing");
+      this.lastHover = new Date();
+    }
+  }
+
+  async checkHoverStatus(): Promise<Boolean> {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const now = new Date();
+        var seconds = (now.getTime() - this.lastHover.getTime()) / 1000;
+        resolve(seconds > 2);
+      }, 2000);
+    });
+  }
+
+  async notShowNextSongInfo(element: HTMLSpanElement){
+    var nextSongInfo = document.getElementById('next-song-info');
+
+    if(await this.checkHoverStatus()){
+      nextSongInfo!.classList.remove("showing");
+      nextSongInfo!.classList.add("not-showing");
+    }
+  }
+
+  setSpeakerAnimation(activate:Boolean){
+    const speaker = document.getElementsByClassName("speaker");
+
+    if(activate){
+      for (var i = 0; i < speaker.length; i++) {
+        speaker[i].classList.add('playing');
+        speaker[i].classList.remove('not-playing');
+      }
+    }else{
+      for (var i = 0; i < speaker.length; i++) {
+        speaker[i].classList.add('not-playing');
+        speaker[i].classList.remove('playing');
+      }
+    }
+
+  }
+
+  selectSong(element: HTMLSpanElement): void{
+    if(this.lights){
+      const currentlyPlayingSongImage = document.getElementById('playing-song-image');
+      const currentSelectedElement = document.getElementById('selected-song');
+
+      //set id to the selected song
+      if(currentSelectedElement != null)
+        currentSelectedElement.id = "";
+      element.id = "selected-song";
+
+      //Change info
+      currentlyPlayingSongImage!.style.backgroundImage ="url('https://m.media-amazon.com/images/I/61YeJkzhAYL._SL1200_.jpg')";
+
+      //Activate Speaker Animation
+      this.setSpeakerAnimation(true);
+    }
+  }
+
+  playSong(image: HTMLElement): void{
+    if(this.lights){
+      if(this.songPlaying){
+        image.classList.remove('paused');
+      }else{
+        image.classList.add('paused');
+      }
+
+      //Deactivate Speaker Animation
+      this.setSpeakerAnimation(this.songPlaying);
+      
+      this.songPlaying = !this.songPlaying;
+    }
   }
 }
